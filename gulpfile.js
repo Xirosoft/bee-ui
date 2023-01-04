f74f65f97127d2dfc8000bb242a8bdf63b260d42
sofiquldev <sofiquldev@gmail.com>
2023-01-04 15:49:48 +0600

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,117 @@
+const prop = require('./package.json');
+const minify = require('gulp-clean-css');
+const sass = require('gulp-sass')(require('sass')); // Use Dart Sass;
+const gulp = require('gulp');
+const $ = require('gulp-load-plugins')();
+const sizereport = require('gulp-sizereport');
+const gzip = require('gulp-gzip');
+const head = '/*\r\n* Bee UI ' + prop.version +
+    `\r\n* © ${new Date().getFullYear()} Xirosoft, \r\n* https://xirosoft.github.io/bee\r\n*/\r\n`;;
+
+generateGulpBuild(`core`, `src/bee-core.scss`, `bee-core`);
+generateGulpBuild(`all`, `src/bee-all.scss`, `bee-all`);
+
+// source file name
+// file name
+function generateGulpBuild(taskName, sassFilePath, outputName) {
+    gulp.task(taskName, () => {
+        return gulp
+            .src([sassFilePath], ['allowEmpty'])
+            .pipe(
+                sass({
+                    includePaths: ['./node_modules'],
+                })
+            )
+            .on('error', sass.logError)
+            .pipe(
+                sass.sync().on('error', function (err) {
+                    sass.logError.call(this, err);
+                })
+            )
+            .pipe($.concat(`${outputName}.css`))
+            .pipe($.header(head))
+            .pipe($.size())
+            .pipe(gulp.dest('./dist/'))
+            .on('error', (err) => {
+                console.error(err);
+                process.exit(1);
+            });
+    });
+
+    gulp.task(
+        `minify-${taskName}`,
+        gulp.series(taskName, () => {
+            return (
+                gulp
+                    .src([`./dist/${outputName}.css`])
+                    .pipe(
+                        minify(
+                            {
+                                level: {
+                                    1: {
+                                        all: true,
+                                        normalizeUrls: false,
+                                    },
+                                    2: {
+                                        all: false,
+                                        removeDuplicateRules: true,
+                                        reduceNonAdjacentRules: true,
+                                        removeDuplicateFontRules: true,
+                                        removeDuplicateMediaBlocks: true,
+                                        mergeAdjacentRules: true,
+                                        mergeIntoShorthands: true,
+                                        mergeMedia: true,
+                                        mergeNonAdjacentRules: true,
+                                        mergeSemantically: false,
+                                        removeEmpty: true,
+                                    },
+                                },
+                            },
+                            (details) => {
+                                console.log('FULL');
+                                console.log(details.name + ': ' + details.stats.originalSize);
+                                console.log(`${outputName}.min.css ${details.stats.minifiedSize}`);
+                            }
+                        )
+                    )
+                    .pipe($.header(head))
+                    .pipe($.size())
+                    .pipe($.concat(`${outputName}.min.css`))
+                    .pipe(gulp.dest('./dist/'))
+                    .on('error', (err) => {
+                        console.error(`Error encountered for task ${taskName}. Failing.`);
+                        process.exit(1);
+                    })
+            );
+        })
+    );
+
+    // Create Gzipped build
+    gulp.task(
+        `gzip-${taskName}`,
+        gulp.series(taskName, `minify-${taskName}`, () => {
+            return (
+                gulp
+                    .src([`./dist/${outputName}.min.css`], ['allowEmpty'])
+                    .pipe(gzip())
+                    .pipe(
+                        sizereport({
+                            gzip: true,
+                            total: false,
+                        })
+                    )
+                    .pipe(gulp.dest('./dist/'))
+                    .on('error', (err) => {
+                        console.error(`Error encountered for task ${taskName}. Failing.`);
+                        process.exit(1);
+                    })
+            );
+        })
+    );
+}
+
+gulp.task('watch', () => gulp.watch('./src/**/*.scss', gulp.parallel('minify-core', 'minify-all')));
+
+gulp.task('default', gulp.parallel('minify-core', 'minify-all'));
+
+gulp.task('gzip', gulp.parallel('gzip-core', 'gzip-all'));
---
+++ readme.md
@@ -0,0 +1,102 @@
+# **Bee UI**
+  [![v.0.0.1](https://img.shields.io/badge/Bee%20UI-0.0.1-blue.svg)](https://github.com/xirosoft/bee-ui)
+  [![MIT License](https://img.shields.io/badge/license-MIT-orange.svg)](https://opensource.org/licenses/MIT)
+
+ **Bee UI** is a lightweight **SCSS** based UI Framework. You can use, manage and contribute with it in easyest way... 
+---
+---
+<br>
+
+## Features
+- **Beautiful Components**
+- **Lightweight**
+- **Responsive**
+- **Zero-dependency**
+
+<br>
+
+## Install
+Shown install method
+
+<br>
+
+## How to Use
+
+```html
+<link rel="stylesheet" href="https://www.xirosoft.com/bee-ui">
+```
+<br>
+
+## Development
+Use Gitpod, a free online dev environment for GitHub.
+
+[![Open in Gitpod](https://gitpod.io/button/open-in-gitpod.svg)](https://gitpod.io/#https://github.com/xirosoft/bee-ui)
+
+Or clone locally:
+```bash
+$ git clone https://github.com/xirosoft/bee-ui.git
+$ cd bee-ui
+$ npm install
+$ npm watch
+```
+<br>
+
+## Folder Structure  of **Bee UI**
+    .
+    ├── dist                        # Compiled files
+    ├── docs                        # Documentation files
+    └── src                         # Source files
+        └── base                    #
+            ├── _index.scss         #
+            ├── aniamtions.scss     #
+            ├── default.scss        #
+            ├── font.scss           #
+            ├── grid.scss           #
+            ├── layout.scss         #
+            ├── media.scss          #
+            ├── modifiers.scss      #
+            └── spacing.scss        #
+        └── builds
+            ├── core.scss           #
+            └── ext.scss            #
+        └── components
+            ├── _index.scss         #
+            └── button.scss         #
+        └── internal
+            ├── _index.scss         #
+            ├── _constants.scss     #
+            ├── _defaults.scss      #
+            ├── _functions.scss     #
+            ├── _mixins.scss        #
+            ├── _selectors.scss     #
+            └── _theme.scss         #
+        └── utils
+            ├── _index.scss         #
+            ├── absolute.scss       #
+            ├── blur.scss           #
+            ├── border.scss         #
+            ├── clearfix.scss       #
+            ├── display.scss        #
+            ├── flex.scss           #
+            ├── misc.scss           #
+            ├── opacity.scss        #
+            ├── overflow.scss       #
+            ├── position.scss       #
+            ├── shadows.scss        #
+            ├── transistions.scss   #
+            └── zindex.scss         #
+        ├── bee-all.scss            #
+        └── bee-core.scss           #
+    ├── gulpfile.js                 #
+    ├── package.json                #
+    └── README.md                   #
+
+<br>
+
+## ❤️ Sponsors and Backers
+Themefy
+
+<br>
+
+## Cradits
+- Folder Stucture : [Cirrus](https://github.com/Spiderpig86/Cirrus)
