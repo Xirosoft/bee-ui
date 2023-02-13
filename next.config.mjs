16e3ab4d1ceca935660c9a3348d9b0e904c1e46e
Sofiqul Islam <sofiquldev@gmail.com>
2023-02-13 17:47:31 +0600

    update action file

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,29 @@
 import nextra from 'nextra'
+import withPlugins from 'next-compose-plugins'
+import nextOptimizedImages from 'next-optimized-images'
 
-const withNextra = nextra({
+const nextraConfig = nextra({
   theme: 'nextra-theme-docs',
   themeConfig: './src/theme.config.js',
-  staticImage: true,
+  staticImage: false,
   latex: true,
   unstable_staticImage: true,
   flexsearch: {
     codeblock: true
   },
   images: {
+    disableStaticImages: true,
     loader: 'akamai',
     path: '',
-    unoptimized:  true
   },
-  assetPrefix: './',
+  assetPrefix: './out',
 })
 
-export default withNextra({
-  reactStrictMode: true
-})
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized:  true,
+  }
+}
+
+export default withPlugins([nextraConfig, nextOptimizedImages], nextConfig)
