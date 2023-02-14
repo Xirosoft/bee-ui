46146bb7299232c775960a360d7c21b968590f7a
Sofiqul Islam <sofiquldev@gmail.com>
2023-02-14 16:00:01 +0600

    action update

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,55 @@
+// import nextra from 'nextra'
+// import withPlugins from 'next-compose-plugins'
+// import nextOptimizedImages from 'next-optimized-images'
+
+// const nextraConfig = nextra({
+//   theme: 'nextra-theme-docs',
+//   themeConfig: './src/theme.config.js',
+//   staticImage: true,
+//   latex: true,
+//   unstable_staticImage: true,
+//   flexsearch: {
+//     codeblock: true
+//   },
+// })
+
+// const nextConfig = {
+//   reactStrictMode: true,
+//   images: {
+//     unoptimized:  true,
+//     disableStaticImages: false,
+//     loader: 'akamai',
+//     path: '/bee-ui/',
+    // remotePatterns: [
+    //   {
+    //     protocol: 'https',
+    //     hostname: `${process.env.GITHUB_REPOSITORY_OWNER}.github.io/`,
+    //     pathname: `${process.env.BASE_PATH}/**`,
+    //   },
+    // ],
+//   },
+//   basePath: process.env.BASE_PATH,
+//   assetPrefix: process.env.BASE_PATH,
+// }
+
+// export default withPlugins([nextraConfig, nextOptimizedImages], nextConfig)
+
+
+// const withNextra = require('nextra')({
+//   theme: "nextra-theme-docs",
+//   themeConfig: "./src/theme.config.js",
+// })
+
+// const nextConfig = {
+//   i18n: {
+//     locales: ['en', 'bn'],
+//     defaultLocale: 'en'
+//   },
+//   basePath: process.env.BASE_PATH,
+// }
+ 
+// module.exports = withNextra(nextConfig)
+
 import * as dotenv from 'dotenv'
 import nextra from 'nextra'
 
@@ -16,6 +68,13 @@ const withNextra = nextra({
 const nextConfig = {
   images: {
     unoptimized : true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: `${process.env.GITHUB_REPOSITORY_OWNER}.github.io/`,
+        pathname: `${process.env.BASE_PATH}/**`,
+      },
+    ],
   },
   basePath: process.env.BASE_PATH,
   assetPrefix: process.env.BASE_PATH,
