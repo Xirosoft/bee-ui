8250e1046b341b6869a85e53ffb450fe9867ba38
Sofiqul Islam <sofiquldev@gmail.com>
2023-02-17 14:27:54 +0600

    font fixed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,14 @@
+import { Inter } from "@next/font/google";
 import 'nextra-theme-docs/style.css'
 import '../../source/beeui.all.scss'
 import '../styles/docs.scss'
 
+const inter = Inter({ subsets: ["latin"] });
+
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={inter.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
