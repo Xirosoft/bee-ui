d14ac741a0016c9aeb552f419bc221b7a724a75e
Shakher Ahmmed <bdsaiket1@gmail.com>
2023-02-22 19:22:16 +0600

    Breadcrumb, Iconbox & infobox update

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import { Inter } from "@next/font/google";
 import 'nextra-theme-docs/style.css'
 import '../../source/beeui.all.scss'
+// import '../styles/beeui.all.css'
 import '../styles/docs.scss'
 
 const inter = Inter({ subsets: ["latin"] });
